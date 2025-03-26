2a7ce73ef7784d9b9945ebb6aeed474a49645235
ferteque <ferteque98@gmail.com>
2025-03-26 23:19:12 +0100

    Create EPG1.sh

---
+++ .github/scripts/EPG1.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+# Variables
+EPG_URL="https://opop.pro/1Fjsu2xxJoDxo"
+OUTPUT_FILE="epg1.xml"
+COMPRESSED_FILE="epg1.xml.gz"
+
+# Descargar el archivo EPG
+wget -O "$OUTPUT_FILE" "$EPG_URL"
+
+# Modificar la etiqueta <tv generator-info-name>
+ sed -i 's|\(<tv generator-info-name="\)[^"]*|\1EPG 1|' "$OUTPUT_FILE"
+ 
+# Comprimir el archivo
+gzip -f "$OUTPUT_FILE"
+
+# Configurar git y subir cambios
+git config --global user.email "ferteque98@gmail.com"
+git config --global user.name "Ferteque"
+
+# Subir cambios a GitHub
+git add "$COMPRESSED_FILE"
+git commit -m "EPG updated on $DATE"
+git push origin main
