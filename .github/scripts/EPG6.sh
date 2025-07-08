db5a6ce3b0da86c224dc299181c9c1ac728ddcc7
ferteque <ferteque98@gmail.com>
2025-07-08 10:13:04 +0200

    Update EPG6.sh

--- .github/scripts/EPG6.sh
+++ .github/scripts/EPG6.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Variables
-EPG_URL="https://opop.pro/scMTS6bq24jntu"
+EPG_URL="https://opop.pro/teSft6Ls13nZT6"
 OUTPUT_FILE="epg6.xml"
 COMPRESSED_FILE="epg6.xml.gz"
 DATE=$(date +"%Y-%m-%d %H:%M:%S")
